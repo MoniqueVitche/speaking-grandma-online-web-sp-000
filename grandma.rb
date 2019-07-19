60f477cd131cf403c5433e8314864572ff5bb2f1
Monique Vitche <mvitche7@gmail.com>
2019-07-19 22:45:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@
 def speak_to_grandma(string)
   if string = "Hi grandma!"
     puts "HUH?! SPEAK UP, SONNY!"
-  else string = "HI GRANDMA"
+  else string.upcase = "HI GRANDMA"
     puts "NO, NOT SINCE 1938!"
-  elsif string = "I LOVE YOU GRANDMA"
+  elsif string.upcase = "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
 end
